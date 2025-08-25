542db9720ec490d9f1f125f0c588e78d31d38231
rapharosse29 <227596315+rapharosse29@users.noreply.github.com>
2025-08-25 10:12:52 -0300

    Add robot control menu exercise script
    pt/BR
    Implementa um programa simples de controle de robô com um menu para avançar, recuar, verificar status e desligar. O script entra em loop até que o usuário decida desligar, tratando entradas inválidas com uma mensagem de erro.
    
    en
    Implements a simple robot control program with a menu for advancing, retreating, checking status, and shutting down. The script loops until the user chooses to shut down, handling invalid input with an error message.

---
+++ Aula_07/exercicio_3.py
@@ -0,0 +1,59 @@
+"""
+Menu de Comandos para um Robô
+- Crie um program que simula o controle de um robô simples com um menu de comandos.
+1- O robô pode estar em uma posição inicial(Você pode usar uma variável para isso, por exemplo, posição 0)
+2- O programa deve exibir um menu com as seguintes opções: 1- Avançar, 2- Recuar, 3- Status, 4- Desligar
+3- Peça ao usuário para escolher um comando.
+4- Com base na escolha, execute a ação correspondente:
+    * Avançar: Adicione um valor da posição do robô
+    * Recuar: Subtraia um valor da posição do robô.
+    * Status: Mostre a posição atual do robô.
+    * Desligar: Encerre o programa.
+5- O menu deve continuar aparecendo após cada comando, até que o usuário escolha a opção "Desligar"
+6- Se o usuário digitar um comando invalido, exiba uma mensagem de erro.
+"""
+import time
+
+print("--- Ola, esse e o menu de controle do robo, escolha uma das opcoes a seguir: ---\n")
+time.sleep(1.5)
+position = 0
+while True:
+
+    print("(1) Avancar\n")
+    time.sleep(0.2)
+    print("(2) Voltar\n")
+    time.sleep(0.2)
+    print("(3) Status\n")
+    time.sleep(0.2)
+    print("(4) Desligar\n\b")
+    time.sleep(0.2)
+
+    opcao = [1, 2, 3, 4]
+    
+
+    opcao = int(input(""))
+    if opcao == 1:
+        time.sleep(2)
+        print("\nAvançando...")
+        position += 1
+        time.sleep(2)
+    elif opcao == 2:
+        time.sleep(2)
+        print("\nRecuando...")
+        position -= 1
+        time.sleep(2)
+    elif opcao == 3:
+        print("\nLoading...")
+        time.sleep(2.5)
+        print(f"\nPosição atual: {position}")
+    elif opcao == 4:
+        print("\nDesligando o robô...")
+        time.sleep(1)
+        print("\nDesligando o robô..")
+        time.sleep(1)
+        print("\nDesligando o robô.")
+        break
+    else:
+        print("\nOpcao invalida...\n")
+
+
