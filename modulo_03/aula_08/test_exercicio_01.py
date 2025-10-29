16f2990c9151dc80a5224715021e9f819740991d
Rapha <rapharosseprofissional@gmail.com>
2025-10-29 11:15:15 -0300

    Add tests for escolher function in exercicio_01
    Introduces a test file for the escolher function, using mocker to patch random.randint and verify expected output and call count.

---
+++ modulo_03/aula_08/test_exercicio_01.py
@@ -0,0 +1,17 @@
+from exercicio_01 import escolher
+import random
+
+
+def test_escolher(mocker):
+    resultado_random = [1, 3, 4, 2, 3, 1]
+    resultado_mock = mocker.patch(
+        "random.randint",
+        side_effect=resultado_random,
+    )
+
+    lista1 = [random.randint(1, 5) for _ in range(3)]
+    lista2 = [random.randint(1, 5) for _ in range(3)]
+
+    resultado = escolher(lista1, lista2)
+    assert resultado == [1, 3]
+    assert resultado_mock.call_count == 6
